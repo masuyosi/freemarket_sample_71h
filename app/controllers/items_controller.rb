c20cf55c5c0e65e0512081bf5efabaf4ae018678
ebuchiT <ebuchi.ebc@icloud.com>
2020-03-18 18:59:56 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :items, only: [:show]
 end
