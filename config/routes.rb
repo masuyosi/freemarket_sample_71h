1db6f83ef6a5a72c83a5e2259c67dcebe5b35486
kozkoz-shio <enkoz15@gmail.com>
2020-03-20 16:10:02 +0900

    フロント・新規会員

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: [:show]
+  resources :profiles,only: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_095419) do
+ActiveRecord::Schema.define(version: 2020_03_20_054251) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
