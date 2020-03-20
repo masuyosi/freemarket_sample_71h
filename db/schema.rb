b33ec9334d8dff2262cfa99fe9e620dd69d03909
kozkoz-shio <enkoz15@gmail.com>
2020-03-20 20:17:04 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2020_03_20_071443) do
     t.index ["image_id"], name: "index_items_on_image_id"
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -56,13 +62,6 @@ ActiveRecord::Schema.define(version: 2020_03_20_071443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-    create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-  
   add_foreign_key "images", "items"
   add_foreign_key "items", "images"
 end
