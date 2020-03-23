7ad687dc49a0090defaa7345dafc4caa4c48f39f
masuyosi <weiss.der.fulss@gmail.com>
2020-03-23 15:45:50 +0900

    Revert "マイグレーションファイル修正"
    This reverts commit 1ffbd42bb941cd3fcd013d19aa5010937aff58a8.

--- db/migrate/20200318095419_devise_create_users.rb
+++ db/migrate/20200318095419_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :nickname,               null: false
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, length: { minimum: 7 }
+      t.string :encrypted_password, null: false, length: { minimum: 7 }: ""
 
       ## Recoverable
       t.string   :reset_password_token
