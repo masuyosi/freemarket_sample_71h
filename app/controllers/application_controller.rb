b5cf439ab529a54e35d01c0c439b33301c7b48f0
masuyosi <weiss.der.fulss@gmail.com>
2020-03-23 19:14:13 +0900

    basic認証の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   # protect_from_forgery with: :exception
-  before_action :basic_auth
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,8 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '18.177.133.178', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
