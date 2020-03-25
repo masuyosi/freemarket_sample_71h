ed54cc40a55bdfc49862b21e255835951300fb70
kozkoz-shio <enkoz15@gmail.com>
2020-03-25 15:22:31 +0900

    マスターに沿って不具合修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,16 +22,16 @@ class ItemsController < ApplicationController
   end
 
 
-  require 'payjp'
+# require 'payjp'
 
-  def purchase
-    Payjp.api_key = sk_test_19e85988f3b7d2e500b71abf
-    Payjp::Charge.create(
-      amount: 809, # 決済する値段
-      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
-      currency: 'jpy'
-    )
-  end
+#   def purchase
+#     Payjp.api_key = sk_test_19e85988f3b7d2e500b71abf
+#     Payjp::Charge.create(
+#       amount: 809, # 決済する値段
+#       card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+#       currency: 'jpy'
+#     )
+#   end
   
   def destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :items, exept: [:index]
-  resources :profiles,only: [:new,:create]
+  resources :profiles,only: [:new,:create,:show]
   resources :orders, only: [:new, :create]
 end
