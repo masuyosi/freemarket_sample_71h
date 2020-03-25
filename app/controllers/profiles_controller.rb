81c2e432bfe3a1e15684f4f035c4b22979c58c08
ebuchiT <ebuchi.ebc@icloud.com>
2020-03-25 19:02:05 +0900

    Revert "Merge branch 'item_show_sarber_' of https://github.com/masuyosi/freemarket_sample_71h into item_show_sarber_"
    This reverts commit 909a84053175a1cf339c02b0b9c1e9b2f43bd82b, reversing
    changes made to 863d24a6d8f6886283c8b4d21f152985a78d1b85.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
- 
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.create
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  validates :nickname, presence: true
-   
-            
 end
