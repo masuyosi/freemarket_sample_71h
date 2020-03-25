36443c6a9520a3639a771a0a586e0f9168e29cf0
kozkoz-shio <enkoz15@gmail.com>
2020-03-25 14:28:02 +0900

    pay.jp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,5 +22,17 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+
+  require 'payjp'
+
+  def purchase
+    Payjp.api_key = sk_test_19e85988f3b7d2e500b71abf
+    Payjp::Charge.create(
+      amount: 809, # 決済する値段
+      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+      currency: 'jpy'
+    )
+  end
   
 end
