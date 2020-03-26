92900ee4fc6c89e9ab89a4df458e52cb64c4b8ba
kozkoz-shio <enkoz15@gmail.com>
2020-03-26 12:30:50 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,19 +21,6 @@ class ItemsController < ApplicationController
   def update
   end
 
-
-# require 'payjp'
-
-#   def purchase
-#     Payjp.api_key = sk_test_19e85988f3b7d2e500b71abf
-#     Payjp::Charge.create(
-#       amount: 809, # 決済する値段
-#       card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
-#       currency: 'jpy'
-#     )
-#   end
-  
   def destroy
-
   end
 end
