c9464686656baf27fa50326cdf9260a154833cff
masuyosi <weiss.der.fulss@gmail.com>
2020-03-26 19:19:00 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
 
   validates_associated :images
   validates :images, presence: true
-  belongs_to :user, foreign_key: 'user_id'
+  belongs_to user, foreign_key: 'user_id'
   belongs_to :category
 
   def image_search
