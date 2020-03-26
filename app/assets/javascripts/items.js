e9319a6916c4c3f34c5fcee82984250766dd42f5
ebuchiT <ebuchi.ebc@icloud.com>
2020-03-26 19:50:13 +0900

    Update items.js

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,7 +2,6 @@ $(function(){
   // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
     const html = `<div data-index="${num}" class="js-file_group">
-                    <p>画像${num}</p>
                     <input class="js-file" type="file"
                     name="item[images_attributes][${num}][src]"
                     id="item_images_attributes_${num}_src">
