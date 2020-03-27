21dfd7012c65e853f2b3af6be6988d6a9f677d96
masuyosi <weiss.der.fulss@gmail.com>
2020-03-27 21:25:32 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20200326121957_remove_phone_number_from_addresses.rb
+++
@@ -1,5 +0,0 @@
-class RemovePhoneNumberFromAddresses < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :addresses, :phone_number, :integer
-  end
-end
--- db/migrate/20200326122120_remove_family_name_from_addresses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFamilyNameFromAddresses < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :addresses, :family_name, :string
-  end
-end
