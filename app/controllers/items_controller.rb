b936906b1157c698503b86346deefe1df6439e49
masuyosi <weiss.der.fulss@gmail.com>
2020-03-31 19:26:20 +0900

    編集のカテゴリー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,25 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
   end
 
   def update
