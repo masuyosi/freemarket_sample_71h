31aa8d8d6dbfac8a36265c2799602ae8eb55cdf3
ebuchiT <ebuchi.ebc@icloud.com>
2020-03-31 14:21:34 +0900

    一覧ページ、詳細ページ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.where("name LIKE ?", "%#{params[:name]}%")
-    @items = Item.all.order("created_at DESC")
-    @images = Image.all.includes(:item)
+    @items = Item.all.order("created_at DESC").limit(6)
   end
 
   def new
@@ -17,12 +16,9 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
-    if @item.save
-      redirect_to root_path
-      flash[:notice] = "出品しました"
-    end
     if @item.update(seller_id: current_user.id)
       flash[:notice] = "出品しました"
+      redirect_to root_path
     else
       flash[:notice] = "出品に失敗しました"
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_161259) do
+ActiveRecord::Schema.define(version: 2020_03_30_161258) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture", null: false
@@ -58,7 +58,8 @@ ActiveRecord::Schema.define(version: 2020_03_30_161259) do
     t.string "name", null: false
     t.text "content", null: false
     t.integer "price", null: false
-    t.integer "seller_id"
+    t.integer "seller_id", null: false
+    t.integer "buyer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "postage_payer_id", null: false
