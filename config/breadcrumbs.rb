053fcac21b245182e252374c46b9b5a95b933ce2
kozkoz-shio <enkoz15@gmail.com>
2020-04-01 18:30:40 +0900

    パンくずリスト実装（追加実装ページについては適宜実装）

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -12,40 +12,17 @@ crumb :logout do
   parent :profile
 end
 
-crumb :CardShow do
+crumb :cardshow do
   link "登録カード情報", card_path
   parent :profile
 end
-
 
 crumb :CardNew do
   link "カード登録", card_path(current_user)
   parent :profile
 end
 
-
-
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :items do
+  link "商品情報", item_path,@item
+  parent :root
+end
