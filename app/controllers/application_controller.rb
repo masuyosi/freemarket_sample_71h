d11089896f481257cbee0a47c93e410a0bbd78d0
masuyosi <weiss.der.fulss@gmail.com>
2020-04-01 12:55:41 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_parents
 
   private
 
@@ -17,5 +18,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def set_parents
+    @parents = Category.all.order("id ASC").limit(13)
   end
 end
